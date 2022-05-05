a59694e144985fb6baa892d6e8980e15d0d25333
David Ferreira <dave.ferreira@broadpointgroup.com>
2022-05-05 11:46:59 -0500

    Initial commit

---
+++ DualOperator/Helpers/LoadOperator.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using DualOperator.Models;
+
+namespace DualOperator.Helpers
+{
+    internal static class LoadOperator
+    {
+        public static List<RunningApp> OperatorApps = new List<RunningApp>();
+        public static void LoadApps()
+        {
+            // First, get the config info
+            List<OperatorApp>? appList = JsonSerializer.Deserialize<List<OperatorApp>>(File.ReadAllText("operators.json"));
+
+            // Sanity check - did we get anything
+            if (appList == null || appList.Count == 0)
+            {
+                return;
+            }
+
+            // Process the first item
+            RunningApp appItem = new RunningApp
+            {
+                Keyboard = appList[0].Keyboard,
+                WindowHandle = IntPtr.Zero,
+                WindowTitle = appList[0].ApplicationTitle
+            };
+            OperatorApps.Add(appItem);
+
+            // And now the second item
+            if (appList.Count <= 1)
+            {
+                return;
+            }
+
+            appItem = new RunningApp
+            {
+                Keyboard = appList[1].Keyboard,
+                WindowHandle = IntPtr.Zero,
+                WindowTitle = appList[1].ApplicationTitle
+            };
+            OperatorApps.Add(appItem);
+        }
+    }
+}
---
+++ DualOperator/Program.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using DualOperator.Helpers;
+
+namespace DualOperator
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Prepare to run
+            ApplicationConfiguration.Initialize();
+
+            // Check for command line arguments
+            if (args.Length > 0)
+            {
+                switch (args[0].ToUpper())
+                {
+                    case "/LIST":
+                        // If we received a file name, use that
+                        Win32.DeviceAudit(args.Length > 1 ? args[1] : @"DeviceAudit.txt");
+                        break;
+
+                    default:
+                        StringBuilder message = new StringBuilder();
+                        message.AppendLine(@"DualOperator takes up to 2 parameters:");
+                        message.AppendLine(@"Parameter 1 is /LIST to produce a list of HID devices on the current machine.");
+                        message.AppendLine(@"Parameter 2 is optional and is the full path and file name for the HID output list.");
+                        message.AppendLine(@"If you do not specify a file name, DeviceAudit.txt will be created in the current directory.");
+                        message.AppendLine(@"Click the OK button to exit.");
+                        MessageBox.Show(message.ToString(), @"Dual Operator", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                        break;
+                }
+
+                // And then get out
+                return;
+            }
+            
+            // Load the configuration file if it exists
+            if (File.Exists("operators.json"))
+            {
+                LoadOperator.LoadApps();
+            }
+
+            // Launch the application
+            Application.Run(new OperatorManager());
+        }
+    }
+}
